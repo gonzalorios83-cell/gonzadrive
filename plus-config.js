8754ff14cae65d47feb43d7ddaf9def8d1a5c16d
gonzalorios83-cell <gonzalorios83@gmail.com>
2026-06-17 15:37:08 -0300

    Fix formatting in plus-config.js

--- plus-config.js
+++ plus-config.js
@@ -2,7 +2,7 @@
 // Pegá acá el Client ID de Google Cloud OAuth 2.0.
 // Sin este dato, la interfaz Plus carga, pero no puede sincronizar con Drive.
 window.PS_PLUS_CONFIG = {
-  GOOGLE_CLIENT_ID: "819470697641-lnqmk4o8quc2f7818k9q5dabk3gn530.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "819470697641-lnqmk4q8quc2f7818k9q5dabk3gn530.apps.googleusercontent.com",
   DRIVE_FILE_NAME: "puntosmart-config.json",
   DRIVE_SCOPE: "https://www.googleapis.com/auth/drive.appdata"
 };
